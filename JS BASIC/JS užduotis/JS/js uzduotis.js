5ec9574662d82fa050709c958b5d79cfa432fb6f
Auriukux <auriukux.info@gmail.com>
2024-10-24 09:25:03 +0300

    Update js uzduotis.js

--- JS BASIC/JS užduotis/JS/js uzduotis.js
+++ JS BASIC/JS užduotis/JS/js uzduotis.js
@@ -4,7 +4,7 @@ const atsargos = [
         "kategorijos": "Informacinės technologijos",
         "knygos": [
             { "title": "Programavimas linksmai", "autorius": "Jonas Skeivys", "ISBN": "IT-12345", "leidybos_metai": 2023, "puslapiu": 250, "kiekis": 12, "kaina": 25 },
-            { "title": "Webo technologijos", "autorius": "Neal Stephenson", "ISBN": "IT-12346", "leidybos_metai": 2022, "puslapiu": 300, "kiekis": 7, "kaina": 30 },
+            { "title": "Webo technologijos", "autorius": "Neal Stephenson", "ISBN": "IT-12346", "leidybos_metai": 2022, "puslapiu": 300, "kiekis": 7, "kaina": 42 },
             { "title": "Algoritmai ir duomenų struktūros", "autorius": "Raimondas Čiegis", "ISBN": "IT-12347", "leidybos_metai": 2023, "puslapiu": 400, "kiekis": 5, "kaina": 40 },
             { "title": "Kibernetinio saugumo pagrindai", "autorius": "Audrius Lopata", "ISBN": "IT-12348", "leidybos_metai": 2021, "puslapiu": 350, "kiekis": 10, "kaina": 35 },
             { "title": "Dirbtinis intelektas", "autorius": "Max Tegmark", "ISBN": "IT-12349", "leidybos_metai": 2023, "puslapiu": 500, "kiekis": 3, "kaina": 50 }
@@ -14,7 +14,7 @@ const atsargos = [
         "kategorijos": "Fantastika",
         "knygos": [
             { "title": "Kopos", "autorius": "Frank Herbert", "ISBN": "F-00001", "leidybos_metai": 1965, "puslapiu": 412, "kiekis": 20, "kaina": 20 },
-            { "title": "Neuromantas", "autorius": "William Gibson", "ISBN": "F-00002", "leidybos_metai": 1984, "puslapiu": 271, "kiekis": 15, "kaina": 18 },
+            { "title": "Neuromantas", "autorius": "William Gibson", "ISBN": "F-00002", "leidybos_metai": 1984, "puslapiu": 271, "kiekis": 15, "kaina": 10 },
             { "title": "Marsietis", "autorius": "Andy Weir", "ISBN": "F-00003", "leidybos_metai": 2011, "puslapiu": 369, "kiekis": 10, "kaina": 22 },
             { "title": "1984", "autorius": "George Orwell", "ISBN": "F-00004", "leidybos_metai": 1949, "puslapiu": 328, "kiekis": 18, "kaina": 14 },
             { "title": "Drąsi nauja pasaulis", "autorius": "Aldous Huxley", "ISBN": "F-00005", "leidybos_metai": 1932, "puslapiu": 288, "kiekis": 14, "kaina": 12 }
@@ -35,9 +35,9 @@ const atsargos = [
         "knygos": [
             { "title": "Enderio žaidimas", "autorius": "Orson Scott Card", "ISBN": "SF-00001", "leidybos_metai": 1985, "puslapiu": 324, "kiekis": 15, "kaina": 21 },
             { "title": "Fondas", "autorius": "Isaac Asimov", "ISBN": "SF-00002", "leidybos_metai": 1951, "puslapiu": 255, "kiekis": 11, "kaina": 19 },
-            { "title": "Pirmasis žaidėjas", "autorius": "Ernest Cline", "ISBN": "SF-00003", "leidybos_metai": 2011, "puslapiu": 374, "kiekis": 13, "kaina": 22 },
+            { "title": "Pirmasis žaidėjas", "autorius": "Ernest Cline", "ISBN": "SF-00003", "leidybos_metai": 2011, "puslapiu": 374, "kiekis": 13, "kaina": 17 },
             { "title": "Autostopu per galaktiką", "autorius": "Douglas Adams", "ISBN": "SF-00004", "leidybos_metai": 1979, "puslapiu": 215, "kiekis": 14, "kaina": 18 },
-            { "title": "Sniego katastrofa", "autorius": "Henrik Strieberg", "ISBN": "SF-00005", "leidybos_metai": 1992, "puslapiu": 470, "kiekis": 9, "kaina": 25 }
+            { "title": "Sniego katastrofa", "autorius": "Henrik Strieberg", "ISBN": "SF-00005", "leidybos_metai": 1992, "puslapiu": 470, "kiekis": 9, "kaina": 28 }
         ]
     },
 ];
@@ -49,7 +49,7 @@ function apskaiciuotiAtsargas(atsargos) {
     let bendraVertė = 0;
     for (let kategorija of atsargos) {
         let kategorijosVertė = 0;
-        console.log(`Kategorija: ${kategorija.kategorijos}`);
+        console.log(`\nKategorija: ${kategorija.kategorijos}`);
 
         for (let knyga of kategorija.knygos) {
             if (knyga.leidybos_metai === dabartiniaiMetai) {
@@ -62,14 +62,14 @@ function apskaiciuotiAtsargas(atsargos) {
 
             let knygosStatusas = knyga.leidimo_metai;
 
-            console.log(`- ${knyga.title} (${knyga.ISBN}) - ${knygosStatusas} - Puslapiai: ${knyga.puslapiu}, Kiekis: ${knyga.kiekis}, Kaina: ${knyga.kaina} EUR, Bendra Knygos Kaina: ${knyga.bendraKaina} EUR`);
+            console.log(`${knyga.title} (${knyga.ISBN}) - ${knygosStatusas} - Puslapiai: ${knyga.puslapiu}, Kiekis: ${knyga.kiekis}, Kaina: ${knyga.kaina} EUR, Bendra Knygos Kaina: ${knyga.bendraKaina} EUR`);
         }
 
-        console.log(`\nBendra vertė ${kategorija.kategorijos}: ${kategorijosVertė} EUR`);
+        console.log(`\n- Bendra vertė ${kategorija.kategorijos}: ${kategorijosVertė} EUR`);
         bendraVertė += kategorijosVertė;
         console.log("---------------------------------------------------");
     }
-    console.log(`Bendra atsargų vertė: ${bendraVertė} EUR`);
+    console.log(`- Bendra atsargų vertė: ${bendraVertė} EUR`);
 }
 
 apskaiciuotiAtsargas(atsargos);
@@ -98,7 +98,7 @@ function filterBooksBy(books, category = null, title = null, minPages = null, ma
 }
 
 const filteredBooks = filterBooksBy(atsargos);
-console.log("Filtruotos knygos pagal kategoriją, pavadinimą ir puslapių:");
+console.log("\nFiltruotos knygos pagal kategoriją, pavadinimą ir puslapių:");
 console.log(filteredBooks);
 
 
@@ -156,21 +156,21 @@ function sortBooksByTitle(books, order = 'asc') {
 function printBooksauthor(bookList, title) {
     console.log(`\n${title}:`);
     bookList.forEach(book => {
-        console.log(`- Autorius: ${book.autorius || 'N/A'}`);
+        console.log(`Autorius: ${book.autorius || 'N/A'}`);
     });
 }
 
 function printBooksprice(bookList, title) {
     console.log(`\n${title}:`);
     bookList.forEach(book => {
-        console.log(`- Kaina: ${book.kaina}`);
+        console.log(`Kaina: ${book.kaina}`);
     });
 }
 
 function printBookstitle(bookList, title) {
     console.log(`\n${title}:`);
     bookList.forEach(book => {
-        console.log(`- Pavadinimas: ${book.title}`);
+        console.log(`Pavadinimas: ${book.title}`);
     });
 }
 
